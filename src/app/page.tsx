964bda27c576c05b8b66bf970e871e6121cfe04d
Asuravann <32295714+Asuravann@users.noreply.github.com>
2025-01-10 01:22:53 +0530

    Update page.tsx. ( forgot to convert to jsx syntax)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-black ">
         <div className="d-flex align-items-center justify-content-center gap-4 bg-white pe-4">
-          <div class="spinner-grow" role="status">
-            <span class="sr-only">Loading...</span>
+          <div className="spinner-grow" role="status">
+            <span className="sr-only">Loading... </span>
           </div>
           <h1 className="display-1 fw-bold mb-1">SanHashira</h1>
         </div>
