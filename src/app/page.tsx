50731cd54f304dc1b967aa8cf0abe81fc8f78621
Asuravann <32295714+Asuravann@users.noreply.github.com>
2025-01-10 01:18:41 +0530

    Update page.tsx growing spinner for image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-black ">
         <div className="d-flex align-items-center justify-content-center gap-4 bg-white pe-4">
-          <img
-           src="https://drive.google.com/uc?export=view&id=10rbuXaTVJ5B48Au3HawCrAh4ixFNohQ1"
-           alt="Logo"
-           className="img-fluid"
-           style={{ maxWidth: "100px", height: "auto" }}
-           />
+          <div class="spinner-grow" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
           <h1 className="display-1 fw-bold mb-1">SanHashira</h1>
         </div>
         <div className="d-flex align-items-center justify-content-center w-100">
